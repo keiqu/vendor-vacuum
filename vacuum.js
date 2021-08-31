b1481a79fc8e62c76e9958424fcd008067975fe6
Kirill Salnikov <salnikov.k54@gmail.com>
2021-08-31 22:29:16 +0300

    add compatibility with chrome

--- vacuum.js
+++ vacuum.js
@@ -1,43 +1,41 @@
-async function vacuum() {
-    let nothingCounter = 0
-    let foundTree = false
+let nothingCounter = 0
+let foundTree = false
 
-    while (true) {
-        if (nothingCounter >= 5) {
-            console.log("exiting")
-            return
-        }
-
-        console.log("vacuuming...")
-
-        if (!foundTree) {
-            vendorFolder = document.querySelector("[data-qa-file-name='vendor']")
-            if (vendorFolder != null) {
-                vendorFolder.parentNode.parentNode.remove()
-                console.log("removed tree entry!")
-                foundTree = true
-            }
-        }
-
-        let elems = document.querySelectorAll("[data-path^='vendor']")
-        if (elems.length == 0) {
-            console.log("vendor not found! sleeping...")
-
-            nothingCounter++
-            await new Promise(r => setTimeout(r, 1000));
-
-            continue
-        }
-
-        nothingCounter = 0
-        elems.forEach((e) => {
-            console.log("removed vendor entry")
-            e.remove()
-        })
-
-        console.log("finished vacuuming!")
-        await new Promise(r => setTimeout(r, 1000));
+function vacuum() {
+    if (nothingCounter >= 10) {
+        console.log("exiting")
+        return
     }
+
+    console.log("vacuuming...")
+
+    if (!foundTree) {
+        vendorFolder = document.querySelector("[data-qa-file-name='vendor']")
+        if (vendorFolder != null) {
+            vendorFolder.parentNode.parentNode.remove()
+            console.log("removed tree entry!")
+            foundTree = true
+        }
+    }
+
+    let elems = document.querySelectorAll("[data-path^='vendor']")
+    if (elems.length == 0) {
+        console.log("vendor not found! sleeping...")
+
+        nothingCounter++
+        setTimeout(vacuum, 1000)
+
+        return
+    }
+
+    nothingCounter = 0
+    elems.forEach((e) => {
+        e.remove()
+    })
+    console.log("removed " + elems.length + " vendor entries.")
+
+    console.log("finished vacuuming!")
+    setTimeout(vacuum, 1000)
 }
 
 console.log("started vacuum app!")
